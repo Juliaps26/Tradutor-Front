951e0ee9a65489ad77d1ee0b9f3c1d2c33fe0cd5
JULIA PEREIRA SILVA <23111810@CFP127ALUNO.LOCAL>
2024-03-26 10:55:23 -0300

    terminado

--- js/modo.js
+++ js/modo.js
@@ -1,9 +1,43 @@
+
 // Ativar modo dark
 const chk = document.getElementById('chk')
 const text = document.getElementById('text')
+const placeholdercor = document.getElementById('placeholdercor')
 
 chk.addEventListener('change', () => {
   document.body.classList.toggle('dark')
   text.classList.toggle('dark-texto')
+  text.classList.toggle('placeholder-cor')
+
+  // Mudar imagem quando ativar o modo escuro 
+  const img = chk.nextElementSibling.children[0].children[0].children[0]
+
+  if(img.src.includes('sol')){
+    img.src = '../img/lua.png'
+  }else{
+    img.src = '../img/sol.png'
+  }
 
 })
+
+
+// Quando digitar alice mudar a cor do background
+const textoDigitado = document.getElementById('textoDigitado')
+
+textoDigitado.addEventListener('input', function(){
+  const inputValue = textoDigitado.value.toLowerCase();
+  const fundoMudar = inputValue.includes('alice');
+
+  if(fundoMudar){
+    document.body.classList.add('fundo-mudar')
+
+  }else{
+    document.body.classList.remove('fundo-mudar')
+  }
+})
+
+
+
+
+
+
